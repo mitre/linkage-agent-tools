fb2e36c4882bd42da46a8af59d53c31b1c66f905
Andrew Pellitieri <apellitieri@mitre.org>
2020-10-13 05:22:04 -0600

    Reduces number of i/o operations

--- match.py
+++ match.py
@@ -1,10 +1,12 @@
 import time
 from dcctools.config import Configuration
 from tinydb import TinyDB
+from tinydb.storages import JSONStorage
+from tinydb.middlewares import CachingMiddleware
 from dcctools.anonlink import Results, Project
 
 c = Configuration("config.json")
-database = TinyDB('results.json')
+database = TinyDB('results.json', storage=CachingMiddleware(JSONStorage))
 for project_name, schema in c.load_schema().items():
   project = Project(project_name, schema, c.systems(), c.entity_service_url())
   project.start_project()
@@ -22,3 +24,5 @@ for project_name, schema in c.load_schema().items():
   results = Results(c.systems(), project_name, result_json)
   print(result_json)
   results.insert_results(database)
+
+database.close()
