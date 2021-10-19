fd450c96326a8df3700822ede2f6892ae0e17370
unknown <greshje.nachc@gmail.com>
2021-10-19 14:38:09 -0400

    added test to enable reproduction of MongoDB inserts (and six-the-same test files)

---
+++ test/linkage-agent/no-drop/generate-link-ids-skipping-drop.py
@@ -0,0 +1,30 @@
+import util.config.config_util as cu
+import util.linkage_agent.linkage_agent_util as lau
+
+
+# ---
+#
+# This test case can be used to trouble shoot MongoDB issues.
+# It basically does the link_id process  but does not delete (drop) the MongoDB records
+# We running it as a main so we can watch it chug away in PyCharm
+#
+# ---
+
+
+def run_test_match():
+    print("\nStarting test...")
+    config = cu.get_config("test-data/envs/perf-testing/six-the-same-1k/config.json")
+    print("Doing validation...")
+    lau.validate(config)
+    print("Doing match...")
+    lau.match(config)
+    print("Doing link_id...")
+    lau.link_id(config)
+    print("Doing data_owner_ids")
+    lau.data_owner_ids(config)
+    print("Done.")
+
+
+if __name__ == "__main__":
+    run_test_match()
+
