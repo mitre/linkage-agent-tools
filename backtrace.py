ef8a424a26bc588d0e1a423ad8f11e4451e10c31
Andy Gregorowicz <andrewg@mitre.org>
2021-11-03 10:42:09 -0400

    Adding a script to backtrace a LINK_ID to the MongoDB record that created it.

---
+++ backtrace.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import time
+import argparse
+import csv
+
+from pymongo import MongoClient
+
+from dcctools.config import Configuration
+
+c = Configuration("config.json")
+client = MongoClient(c.mongo_uri)
+database = client.linkage_agent
+
+parser = argparse.ArgumentParser(
+    description="Tool for tracing a LINKID back to its matching record in MongoDB"
+)
+parser.add_argument(
+    "linkid",
+    help="The LINKID to find",
+)
+args = parser.parse_args()
+
+result_csv_path = Path(c.matching_results_folder) / "link_ids.csv"
+
+link_id_row = None
+with open(result_csv_path) as csvfile:
+    reader = csv.DictReader(csvfile)
+    link_id_row = next(filter(lambda r: r['LINK_ID'] == args.linkid, reader))
+
+query = {}
+for (key, value) in link_id_row.items():
+    if key != 'LINK_ID' and len(value) > 0:
+      query[key] = int(value)
+
+for result in database.match_groups.find(query):
+    print(result)
