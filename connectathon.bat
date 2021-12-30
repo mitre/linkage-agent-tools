c4313541d37ef3ee7269a03519c3e20f7354043d
NACHC-CAD <greshje.nachc@gmail.com>
2021-12-30 04:43:00 -0500

    Added connectathon script

---
+++ connectathon.bat
@@ -0,0 +1,22 @@
+:: ------
+::
+:: bat file to run time tests
+::
+:: To test this process move the folder ./test-data/time-test/test-set-000
+:: C:\test\test-set-000 then run this from the cmd line
+::
+:: ------
+
+@echo off
+echo.
+echo. 
+echo Calling python process:
+@echo on
+python time_test.py --dir C:\test\connectathon
+@echo off
+echo.
+echo.
+echo Done.  
+echo.
+echo.
+
